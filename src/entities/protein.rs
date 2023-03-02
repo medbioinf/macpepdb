ea6e5229ddb2479a93bf0b4b7bd2d94573a51674
Dirk Winkelhardt <dirk.winkelhardt@rub.de>
2023-03-02 09:48:01 +0100

    add protein

---
+++ src/entities/protein.rs
@@ -0,0 +1,105 @@
+/// Keeps all data from the original UniProt entry which are necessary for MaCPepDB
+/// 
+pub struct Protein {
+    accession: String,
+    secondary_accessions: Vec<String>,
+    entry_name: String,
+    name: String,
+    genes: Vec<String>,
+    taxonomy_id: i64,
+    proteome_id: String,
+    is_reviewed: bool,
+    sequence: String,
+    updated_at: i64
+}
+
+impl Protein {
+    /// Creates a new protein
+    /// 
+    /// # Arguments
+    /// * `accession` - The primary accession
+    /// * `secondary_accessions` - The secondary accessions
+    /// * `entry_name` - The entry name
+    /// * `name` - The protein name
+    /// * `genes` - The genes name
+    /// * `taxonomy_id` - The taxonomy ID
+    /// * `proteome_id` - The proteome ID
+    /// * `is_reviewed` - True if the protein is reviewed (contained by SwissProt)
+    /// * `sequence` - The amino acid sequence
+    /// * `updated_at` - The last update date as unix timestamp
+    /// 
+    pub fn new(accession: String, secondary_accessions: Vec<String>, entry_name: String, name: String, genes: Vec<String>, taxonomy_id: i64, proteome_id: String, is_reviewed: bool, sequence: String, updated_at: i64) -> Self {
+        Self {
+            accession,
+            secondary_accessions,
+            entry_name,
+            name,
+            genes,
+            taxonomy_id,
+            proteome_id,
+            is_reviewed,
+            sequence,
+            updated_at
+        }
+    }
+
+    /// Returns the primary accession
+    /// 
+    pub fn get_accession(&self) -> &String {
+        &self.accession
+    }
+
+    /// Returns the secondary accessions
+    /// 
+    pub fn get_secondary_accessions(&self) -> &Vec<String> {
+        &self.secondary_accessions
+    }
+
+    /// Returns the entry name
+    /// 
+    pub fn get_entry_name(&self) -> &String {
+        &self.entry_name
+    }
+
+    /// Returns the protein name
+    /// 
+    pub fn get_name(&self) -> &String {
+        &self.name
+    }
+
+    /// Returns the gene
+    /// 
+    pub fn get_genes(&self) -> &Vec<String> {
+        &self.genes
+    }
+
+    /// Returns the taxonomy ID
+    /// 
+    pub fn get_taxonomy_id(&self) -> &i64 {
+        &self.taxonomy_id
+    }   
+
+    /// Returns the proteome ID
+    /// 
+    pub fn get_proteome_id(&self) -> &String {
+        &self.proteome_id
+    }
+
+    /// Returns true if the protein is reviewed (contained by SwissProt)
+    /// 
+    pub fn get_is_reviewed(&self) -> bool {
+        self.is_reviewed
+    }
+
+    /// Returns the amino acid sequence
+    /// 
+    pub fn get_sequence(&self) -> &String {
+        &self.sequence
+    }
+
+    /// Returns the last update date as unix timestamp
+    /// 
+    pub fn get_updated_at(&self) -> i64 {
+        self.updated_at
+    }
+}
