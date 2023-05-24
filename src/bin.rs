38c39f0216b0632960bb2685b47562f35e896959
kulgg <75735874+kulgg@users.noreply.github.com>
2023-05-24 17:08:25 +0200

    Add vscode formatter

--- src/bin.rs
+++ src/bin.rs
@@ -1,13 +1,27 @@
-use log::info;
+use std::path::PathBuf;
+
 use clap::{Parser, Subcommand};
+use log::info;
+use macpepdb::{
+    database::citus::database_build::DatabaseBuild, entities::configuration::Configuration,
+};
 
 #[derive(Debug, Subcommand)]
 enum Commands {
     LETSGO {
         name: String,
     },
+    Build {
+        database_url: String,
+        protein_file_paths: Vec<PathBuf>,
+        num_threads: usize,
+        num_partitions: u64,
+        allowed_ram_usage: f64,
+        partitioner_false_positive_probability: f64,
+        show_progress: bool,
+        verbose: bool,
+    },
 }
-
 
 #[derive(Debug, Parser)]
 #[command(name = "macpepdb")]
@@ -15,7 +29,6 @@ struct Cli {
     #[command(subcommand)]
     command: Commands,
 }
-
 
 fn main() {
     env_logger::init();
@@ -26,6 +39,30 @@ fn main() {
     match args.command {
         Commands::LETSGO { name } => {
             info!("Hello, {}", name);
+        }
+        Commands::Build {
+            database_url,
+            protein_file_paths,
+            num_threads,
+            num_partitions,
+            allowed_ram_usage,
+            partitioner_false_positive_probability,
+            show_progress,
+            verbose,
+        } => {
+            let builder = DatabaseBuild::new(database_url);
+            builder
+                .build(
+                    &protein_file_paths,
+                    num_threads,
+                    num_partitions,
+                    allowed_ram_usage,
+                    partitioner_false_positive_probability,
+                    None,
+                    show_progress,
+                    verbose,
+                )
+                .unwrap();
         }
     }
 }
