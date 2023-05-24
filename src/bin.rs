4f0f6d2c60a92ad324573b7b13c19ce30ebbc4f3
kulgg <75735874+kulgg@users.noreply.github.com>
2023-05-24 16:50:12 +0200

    Setup CLI and logging

--- src/bin.rs
+++ src/bin.rs
@@ -1,3 +1,31 @@
+use log::info;
+use clap::{Parser, Subcommand};
+
+#[derive(Debug, Subcommand)]
+enum Commands {
+    LETSGO {
+        name: String,
+    },
+}
+
+
+#[derive(Debug, Parser)]
+#[command(name = "macpepdb")]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+
 fn main() {
-    println!("Welcome to MaCPepDB!");
+    env_logger::init();
+    info!("Welcome to MaCPepDB!");
+
+    let args = Cli::parse();
+
+    match args.command {
+        Commands::LETSGO { name } => {
+            info!("Hello, {}", name);
+        }
+    }
 }
