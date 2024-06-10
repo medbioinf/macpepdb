8d25023f1324c87dec65e692ec60202370a956b5
Dirk Winkelhardt <dirk.winkelhardt@rub.de>
2024-06-10 15:58:32 +0000

    handle error separatly

--- src/bin_performance.rs
+++ src/bin_performance.rs
@@ -181,7 +181,7 @@ async fn web_searching(
 
         while !was_successful {
             was_successful = true;
-            let peptide_stream = client
+            let peptide_response = client
                 .post(&endpoint)
                 .header("Connection", "close")
                 .header("Accept", "text/plain")
@@ -193,10 +193,19 @@ async fn web_searching(
                     "modifications": &ptms,
                 }))
                 .send()
-                .await?
-                .bytes_stream();
+                .await?;
 
-            let peptide_stream = peptide_stream.map(|result| {
+            if !peptide_response.status().is_success() {
+                error!(
+                    "Request was not success: {}, {:?} => Retry",
+                    mass,
+                    peptide_response.text().await?
+                );
+                was_successful = false;
+                continue;
+            }
+
+            let peptide_stream = peptide_response.bytes_stream().map(|result| {
                 result.map_err(|err| std::io::Error::new(std::io::ErrorKind::Other, err))
             });
             let mut read = StreamReader::new(peptide_stream);
