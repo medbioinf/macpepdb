6f80fcfcf12735ea40a676f7cfd58b249aab1cb9
Dirk Winkelhardt <dirk.winkelhardt@rub.de>
2025-02-14 20:02:44 +0100

    clippy cleanup

--- src/entities/peptide.rs
+++ src/entities/peptide.rs
@@ -26,63 +26,58 @@ where
     /// Returns the mass partition
     ///
     pub fn get_partition(&self) -> i64 {
-        return self.partition;
+        self.partition
     }
 
     /// Returns the mass
     pub fn get_mass(&self) -> f64 {
-        return self.mass;
+        self.mass
     }
 
     /// Returns the sequence
     pub fn get_sequence(&self) -> &String {
-        return &self.sequence;
+        &self.sequence
     }
 
     /// Returns the number of missed cleavages
     pub fn get_missed_cleavages(&self) -> i16 {
-        return self.missed_cleavages;
+        self.missed_cleavages
     }
 
     /// Returns the amino acid counts
     ///
     pub fn get_aa_counts(&self) -> &Vec<i16> {
-        return &self.aa_counts;
-    }
-
-    /// Returns the containing proteins
-    pub fn get_proteins(&self) -> &Vec<T> {
-        return &self.proteins;
+        &self.aa_counts
     }
 
     /// Returns true if the peptide is contained in a Swiss-Prot protein
     ///
     pub fn get_is_swiss_prot(&self) -> bool {
-        return self.is_swiss_prot;
+        self.is_swiss_prot
     }
 
     /// Returns true if the peptide is contained in a TrEMBL protein
     ///
     pub fn get_is_trembl(&self) -> bool {
-        return self.is_trembl;
+        self.is_trembl
     }
 
     /// Returns the taxonomy IDs
     ///
     pub fn get_taxonomy_ids(&self) -> &Vec<i64> {
-        return &self.taxonomy_ids;
+        &self.taxonomy_ids
     }
 
     /// Returns the unique taxonomy IDs
     ///
     pub fn get_unique_taxonomy_ids(&self) -> &Vec<i64> {
-        return &self.unique_taxonomy_ids;
+        &self.unique_taxonomy_ids
     }
 
     /// Returns the proteome IDs
     ///
     pub fn get_proteome_ids(&self) -> &Vec<String> {
-        return &self.proteome_ids;
+        &self.proteome_ids
     }
 
     /// Takes the proteins, leaving an empty vector
