bf7630eea2f039f87cfc91375b3f816e6a316b34
Dirk Winkelhardt <dirk.winkelhardt@rub.de>
2025-09-05 12:41:16 +0000

    update readme name

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 // # Create absolute path to readme ti increase compatible for different build targets
 //  https://gist.github.com/JakeHartnell/2c1fa387f185f5dc46c9429470a2e2be
-#![doc = include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/Readme.md"))]
+#![doc = include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/README.md"))]
 
 // 3rd party imports
 #[allow(unused_imports)]
